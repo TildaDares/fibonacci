1b393505b10b70dc98dfb085edd4494fb881bd9d
Tilda Udufo <mathildaudufo@gmail.com>
2020-06-24 23:01:14 +0100

    Minor

--- fibonacci.rb
+++ fibonacci.rb
@@ -2,24 +2,24 @@ def fib(n)
     first_num = 0
     second_num = 1
     third_number = 0
-  n.times do
-    print "#{second_num} " 
+    n.times do
+      print "#{second_num} " 
       third_number = first_num + second_num
       first_num = second_num
       second_num = third_number
-  end
+    end
 end
 
 def fib_rec(n)
-  n < 2 ? n : fib_rec(n-1) + fib_rec(n-2)
+    n < 2 ? n : fib_rec(n-1) + fib_rec(n-2)
 end
 
 def main(n)
-  for i in (1..n)
-    print fib_rec(i)
-    print ' '
-  end
-  puts "\n"
+    for i in (1..n)
+      print fib_rec(i)
+      print ' '
+   end
+    puts "\n"
 end
 
  main(10)
