e8c7f1bdc09b9d15949fdc750d9031016de55e3d
Tilda Udufo <mathildaudufo@gmail.com>
2020-07-19 15:59:34 +0100

    Refactore

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,26 +1,28 @@
+# frozen_string_literal :true
+
 def fib(n)
-    first_num = 0
-    second_num = 1
-    third_number = 0
-    n.times do
-      print "#{second_num} " 
-      third_number = first_num + second_num
-      first_num = second_num
-      second_num = third_number
-    end
+  first_num = 0
+  second_num = 1
+  third_number = 0
+  n.times do
+    print "#{second_num} "
+    third_number = first_num + second_num
+    first_num = second_num
+    second_num = third_number
+  end
 end
 
 def fib_rec(n)
-    n < 2 ? n : fib_rec(n-1) + fib_rec(n-2)
+  n < 2 ? n : fib_rec(n - 1) + fib_rec(n - 2)
 end
 
 def main(n)
-    for i in (1..n)
-      print fib_rec(i)
-      print ' '
-   end
-    puts "\n"
+  (1..n).each do |i|
+    print fib_rec(i)
+    print ' '
+  end
+  puts "\n"
 end
 
- main(10)
- fib(5)
+main(10)
+fib(5)
